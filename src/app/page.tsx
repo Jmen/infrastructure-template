3b6d10f53d6aee74e8e2236d909b510e1f317189
Jaimen Lathia <5757596+Jmen@users.noreply.github.com>
2024-05-08 22:45:12 +0100

    ci: testing latest preview environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
             <SignIn/>
             <div className="flex flex-col rounded-md bg-neutral-100">
                 <div className="p-4 font-bold rounded-t-md bg-neutral-200">
-                    Current Session
+                    Current Session - testing latest preview environment
                 </div>
                 <pre className="py-6 px-4 whitespace-pre-wrap break-all">
                     {JSON.stringify(session, null, 2)}
