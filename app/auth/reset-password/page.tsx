460e23c718605234d190e9643cff0f8679bd5a9f
Jaimen Lathia <5757596+Jmen@users.noreply.github.com>
2025-01-04 21:00:33 +0000

    fix: password reset standalone page

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,10 +1,17 @@
-import { ResetPasswordForm } from "@/components/auth/resetPasswordForm"
+"use server"
 
-export default function Page() {
+import { ResetPasswordForm } from "@/components/auth/resetPasswordForm"
+import { createClient } from "@/lib/supabase/server";
+
+export default async function Page() {
+    const supabase = await createClient();
+
+    const { data: { user } } = await supabase.auth.getUser();
+
     return (
         <div>
             <div className="flex flex-col items-center justify-between gap-6 p-12">
-                <ResetPasswordForm />
+                <ResetPasswordForm user={user} />
             </div>
         </div>
     )
