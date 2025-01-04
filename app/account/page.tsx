a101e95d082b52ca9014b7291d347f6ed7dda172
Jaimen Lathia <5757596+Jmen@users.noreply.github.com>
2025-01-04 19:51:01 +0000

    feat: only show password reset if email auth provider was used

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,25 +1,25 @@
-import { redirect } from "next/navigation";
+"use server"
+
 import UserDetails from "@/components/userDetails";
 import { ResetPasswordForm } from "@/components/auth/resetPasswordForm";
 import { SignOutButton } from "@/components/auth/signOutButton";
 import { createClient } from "@/lib/supabase/server";
+import { User } from "@supabase/auth-js";
 
 export default async function Page() {
     const supabase = await createClient();
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
+    const { data: { user } } = await supabase.auth.getUser();
 
-    if (!user) {
-        return redirect("/");
+    function hasUsedEmailProvider(user: User | null) {
+        return user?.app_metadata?.providers?.includes("email");
     }
 
     return (
         <div>
             <div className="flex flex-col items-center justify-between gap-6 p-12">
                 <UserDetails/>
-                <ResetPasswordForm/>
+                {hasUsedEmailProvider(user) && <ResetPasswordForm/>}
                 <SignOutButton/>
             </div>
         </div>
